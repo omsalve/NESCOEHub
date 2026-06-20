19d276011c95d42a6815710b948693c8e58ee7fe
Om Salve <realomsalve@gmail.com>
2026-06-20 11:42:00 +0530

    chore: scaffold Next.js project with TypeScript, Tailwind and tooling config

---
+++ src/app/layout.tsx
@@ -0,0 +1,38 @@
+import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "NESCOE Hub",
+  description: "Official Hub for NESCOE Students and Faculty",
+  icons: {
+    icon: '/images/NESCOEmulti2grad-removebg-preview.png',
+  },
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        {/* You need to render the children here */}
+        {children}
+      </body>
+    </html>
+  );
+}
